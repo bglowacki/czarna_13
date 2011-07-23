810c17c21ab64c02614968f6ace5351ae5c137d1
Bartek <marathone@gmail.com>
2011-07-23 15:19:23 +0200

    layou change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :load_article_categories, :load_article_sub_categories, :load_scout_teams, :calendar_events_list
+  before_filter :load_article_sub_categories, :load_scout_teams, :calendar_events_list
   
   
   def load_scout_teams
     @scout_teams = ScoutTeam.all
   end
   
-  def load_article_categories
-    @article_categories = ArticleCategory.all
-  end
+  #def load_article_categories
+  #  @article_categories = ArticleCategory.all
+  #end
   
   def load_article_sub_categories
     @article_sub_categories = ArticleSubCategory.all
