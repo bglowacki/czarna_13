893dbca487111cce52a34462bbbd5926abddf3ca
Bartek <marathone@gmail.com>
2011-07-23 11:09:45 +0200

    changes in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,15 @@ set :repository,  "git@github.com:Vangerdahast/czarna_13.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+
+#set :gateway, "verbum-rails.megiteam.pl"
 set :user, "verbum-rails"
 set :use_sudo, false
 set :deploy_to, "/home/verbum-rails/www/#{application}"
 set :deploy_via, :remote_cache
 
 role :web, "verbum-rails.megiteam.pl"                          # Your HTTP server, Apache/etc
-role :app, "verbum-rails.megiteam.pl"                          # This may be the same as your `Web` server
+role :app, "verbum-rails.megiteam.pl"                         # This may be the same as your `Web` server
 role :db,  "verbum-rails.megiteam.pl", :primary => true # This is where Rails migrations will run
 
 
@@ -19,17 +21,12 @@ role :db,  "verbum-rails.megiteam.pl", :primary => true # This is where Rails mi
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-after "deploy", "deploy:bundle_gems"
-after "deploy:bundle_gems", "deploy:restart"
+#after "deploy", "deploy:bundle_gems"
+#after "deploy:bundle_gems", "deploy:restart"
 
-namespace :deploy do
-  task :bundle_gems do 
-   run "cd #{deploy_to}/current && bundle install"  
-end
-  #task :start do ; end
-  #task :stop do ; end
-  
-  #task :restart, :roles => :app, :except => { :no_release => true } do
-  #  run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  #end
-end
+#namespace :deploy do
+#  desc "Restart aplikacji przy pomocy skryptu Megiteam"
+#  task :restart, :role => :app do
+#    run "restart-app #{ application }"
+#  end
+#end
