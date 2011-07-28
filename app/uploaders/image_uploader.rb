c5232acd419678f637fd82571059d011c80b221c
Bartek <marathone@gmail.com>
2011-07-28 17:54:10 +0200

    symlink test

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -51,7 +51,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [140, 140]
   end
   
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
