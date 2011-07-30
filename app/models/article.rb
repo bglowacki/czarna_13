0cdda4db3ce29cfc7d0ecc877fc421cd6b0ab1d8
Bartek <marathone@gmail.com>
2011-07-31 01:37:36 +0200

    errors removed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :article_sub_category
+  
+  validates :title, :presence => true
+  validates :body, :presence => true
+  validates :author, :presence => true
 end
--- app/models/scout_team.rb
+++ app/models/scout_team.rb
@@ -1,5 +1,6 @@
 class ScoutTeam < ActiveRecord::Base
   has_many :scout_team_members
   
-  #scope :came_into_being_order, order => 'came_into_being ASC'
+  validates :name, :presence => true
+  
 end
