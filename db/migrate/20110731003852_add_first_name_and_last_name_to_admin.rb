f2fd7030b9c7671afa1b548afc2209bfd330da5d
Bartek <marathone@gmail.com>
2011-07-31 03:53:35 +0200

    admin link added

--- db/migrate/20110731003852_add_first_name_and_last_name_to_admin.rb
+++ db/migrate/20110731003852_add_first_name_and_last_name_to_admin.rb
@@ -4,6 +4,7 @@ class AddFirstNameAndLastNameToAdmin < ActiveRecord::Migration
     add_column :admins, :last_name, :string
   end
 
+
   def self.down
     remove_column :admins, :last_name
     remove_column :admins, :first_name
