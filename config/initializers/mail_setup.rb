2de46531774fbf8f06fef6165a776f5e769645d7
Bartek <marathone@gmail.com>
2011-08-12 17:32:59 +0200

    mailer configuration put in initializers

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,10 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :domain => 'www.czarna-13.org',
-  :user_name => 'bglowacki@czarna-13.org',
+  #:domain => 'www.czarna-13.org',
+  :user_name => 'marathone@gmail.com',
   :password => 'Bart1398#',
-  :authentication => :login,
+  :authentication => 'plain',
   :enable_starttls_auto => true,
-  :tls => true
 }
