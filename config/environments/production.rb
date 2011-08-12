662f1d8eacd24a44e4a3f589ed52631755fccc66
Bartek <marathone@gmail.com>
2011-08-12 12:02:14 +0200

    mailer config changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,13 +47,13 @@ Czarna13::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'czarna-13.org' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmail.com",
+    :domain               => "czarna-13.org",
     :user_name            => "marathone@gmail.com",
     :password             => "Bartolomeo1398#",
     :authentication       => "plain",
