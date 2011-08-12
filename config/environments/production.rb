bdb38197492723cb095ffcc44ce788daa06ac9dd
Bartek <marathone@gmail.com>
2011-08-12 14:26:15 +0200

    mailer configuration put in initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ Czarna13::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'czarna-13.org' }
+  config.action_mailer.default_url_options = { :host => 'www.czarna-13.org' }
 end
--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :domain => 'gmail.com',
-  :user_name => 'marathone',
-  :password => 'Bartolomeo1398#',
+  :domain => 'www.czarna-13.org',
+  :user_name => 'bglowacki@czarna-13.org',
+  :password => 'Bart1398#',
   :authentication => 'plain',
   :enable_starttls_auto => true
   
