ed2cfac7c94ba2190f90fe787ebd20b6d9496756
Bartek <marathone@gmail.com>
2011-08-22 09:24:57 +0200

    mail port changed

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
-  :port => 25,
+  :port => 587,
   :domain => 'czarna-13.org',
   :user_name => 'czarna.trzynastka@czarna-13.org',
   :password => 'czarna1398',
