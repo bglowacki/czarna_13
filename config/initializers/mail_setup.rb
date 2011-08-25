66bcdc659a9810df10f01869e0977dd074b7ecae
Bartek <marathone@gmail.com>
2011-08-25 18:32:43 +0200

    smtp changed

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,12 +1,12 @@
-#ActionMailer::Base.delivery_method = :smtp 
+ActionMailer::Base.delivery_method = :smtp 
 
 
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :domain => 'czarna-13.org',
-  :user_name => 'czarna.trzynastka@czarna-13.org',
-  :password => 'czarna1398',
+  #:domain => 'czarna-13.org',
+  :user_name => 'marathone@gmail.com',
+  :password => 'Bartolomeo1398#',
   :authentication => :plain,
   :enable_starttls_auto => true
 }
