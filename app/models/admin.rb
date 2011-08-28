f91451059017a7dbb185d89f8d51028c10f7048d
Bartek <marathone@gmail.com>
2011-08-28 11:39:30 +0200

    confirmable option removed

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable,  :recoverable, , :confirmable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable,  :recoverable, , :confirmable, :registerable
   devise :database_authenticatable, 
-          :rememberable, :trackable, :validatable, :registerable
+          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
