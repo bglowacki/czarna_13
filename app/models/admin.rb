b529f03903cf73486891ddf7bbda5ece7d23c5b7
Bartek <marathone@gmail.com>
2011-08-28 23:14:24 +0200

    gallery icon added

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable,  :recoverable, , :confirmable, :registerable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
           :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
