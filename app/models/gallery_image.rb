fda0e9f01d4ad0a710e0b826e5aaf9c816c14f90
Bartek <marathone@gmail.com>
2011-09-05 13:39:35 +0200

    styles changed

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -1,4 +1,4 @@
 class GalleryImage < ActiveRecord::Base
   belongs_to :gallery
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => {:thumb => "150x150>" }
 end
