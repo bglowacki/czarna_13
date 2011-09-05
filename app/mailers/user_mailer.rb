d2f0037d21307bb62ee55007f84d8fbb543f745c
Bartek <marathone@gmail.com>
2011-09-05 11:27:54 +0200

    d

--- app/mailers/user_mailer.rb
+++
@@ -1,6 +0,0 @@
-class UserMailer < ActionMailer::Base
-  
-  def registration_confirmation(user)
-    mail(:to => user.email, :subject => "Zarejestrowano", :from => "czarna.trzynastka@czarna-13.org")
-  end
-end
